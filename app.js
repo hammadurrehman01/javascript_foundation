1685b242924ce9db445738f555bd0057df40ff8f
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-03-17 10:10:23 +0500

    Refactor app.js by commenting out unused code and adding split functionality; update index.html to change background color

--- app.js
+++ app.js
@@ -42,11 +42,58 @@ const nums = [1, 2, 3, 4, 5, 6];
 
 // now finding max value by reduce function
 
-const result = nums.reduce((acc, curr) => {
-  if (curr > acc) {
-    acc = curr;
-  }
-  return acc;
-}, 0);
+// const result = nums.reduce((acc, curr) => {
+//   if (curr > acc) {
+//     acc = curr;
+//   }
+//   return acc;
+// }, 0);
 
-console.log(result);
+// console.log(result);
+
+// function callApi(method) {
+//   return function (url) {
+//     console.log(`Fetching data from ${url} method is: ${method}`);
+//   };
+// }
+
+// const a = callApi("http://localhost:3000");
+// console.log(("GET"))
+
+// function count() {
+//   let num = 0;
+//   num++;
+//   console.log(num);
+// }
+
+// count(); // 1
+// count(); // 1
+
+// function count() {
+//   let num = 0;
+
+//   return function () {
+//     num++;
+//     console.log(num);
+//   };
+// }
+
+
+// const result = count()
+// result()
+// result()
+
+
+///// JOIN /////
+// const words = ["Hello", "world", "JavaScript"];
+
+// const sentence = words.join(" "); // Join with space
+// console.log(sentence); 
+
+
+
+///// SPLIT /////
+const sentence = "Hello world JavaScript";
+
+const words = sentence.split(" "); // Split by space
+console.log(words); 
